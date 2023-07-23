3cf26fc7ac45fc22ad047c082ee7310f46539c7c
br3nr <repetitive_routine@protonmail.com>
2023-07-23 14:01:00 +0800

    Implement slow forward role of wheels

--- rov_arduino/rov_arduino.ino
+++ rov_arduino/rov_arduino.ino
@@ -1,30 +1,15 @@
+// L FORWARD: digitalWrite(2,HIGH);
+// L BACKWARD: analogWrite(5,100);
+// R FORWARD: digitalWrite(4,HIGH);
+// R BACKWARD: analogWrite(6,100);
+
 void setup(){
   pinMode(4, OUTPUT);
   pinMode(2, OUTPUT);
 }
 
 void loop(){
-  digitalWrite(4,HIGH);
-  analogWrite(5,100);
-  digitalWrite(2,HIGH);
-  analogWrite(9,100);
-  delay(2000);
-  digitalWrite(4,LOW);
-  analogWrite(5,100);
-  digitalWrite(2,LOW);
-  analogWrite(9,100);
-  delay(2000);
-  digitalWrite(4,LOW);
-  analogWrite(5,100);
-  digitalWrite(2,HIGH);
-  analogWrite(9,100);
-  delay(2000);
-  digitalWrite(4,HIGH);
-  analogWrite(5,100);
-  digitalWrite(2,LOW);
-  analogWrite(9,100);
-  delay(2000);
-  analogWrite(5,0);
-  analogWrite(9,0);
-  delay(2000);
+  analogWrite(6,50);
+  analogWrite(5,50);
+  //delay(2000);
 }
